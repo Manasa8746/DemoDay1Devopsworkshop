ae3be026e3df5d39b4740fe42b614d33ff0441f4
Manasa M K <manasamk2005@gmail.com>
2026-03-10 20:46:59 +0530

    Day 1 DevOps demo: basics and Git/GitHub workflow
    - Code exercises included
    - Long-term project process explained
    - Practical version control examples"

---
+++ DevOps/Negation.java
@@ -0,0 +1,8 @@
+public class Negation{
+    public static void main(String[] args){
+        int a = ~4;
+        int res = a;
+        System.out.println("res" +res);
+
+    }
+}
---
+++ DevOps/StringComaper.java
@@ -0,0 +1,14 @@
+public class StringComaper{
+    public static void main(String[] args){
+        String str1 = "hello";
+        String str2 = "hello";
+        String str3 = new String("hello");
+        String str4 = new String("hello");
+        System.out.println(str1 == str2);
+        System.out.println(str1 == str3);
+        System.out.println(str3 == str4);
+        System.out.println(str2 == str4);
+        System.out.println(str1.equals(str2));
+        System.out.println(str1.equals(str3));
+    }
+}
